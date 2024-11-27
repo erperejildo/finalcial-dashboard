e5f88eca90260bd0df376e4c0763b6e002ff7353
Daniel <d15_1_89@msn.com>
2024-11-27 11:19:07 +0100

    default routes and main style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './App.scss';
+// src/App.tsx
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Settings from './pages/Settings';
+import './styles/main.scss';
 
-function App() {
-  return <div className="App">Finalcial App</div>;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
