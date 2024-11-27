e3413f748dc156221c62ef7d269b79adfa539c6b
Daniel <d15_1_89@msn.com>
2024-11-27 11:16:05 +0100

    Dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <p>Welcome to your financial overview!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
