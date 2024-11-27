9196595b83171b21062dd613c62293d634ff0239
Daniel <d15_1_89@msn.com>
2024-11-27 11:18:02 +0100

    Settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return (
+    <div className="settings">
+      <h1>Settings Page</h1>
+      <p>Here you can edit your profile, preferences, and security settings.</p>
+    </div>
+  );
+};
+
+export default Settings;
