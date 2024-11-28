dc4d2deb3419b581c3fca87eb779a5c174bcef73
Daniel <d15_1_89@msn.com>
2024-11-28 22:59:28 +0100

    ExpenseStatistics final style

--- src/components/ExpenseStatistics.tsx
+++ src/components/ExpenseStatistics.tsx
@@ -6,29 +6,64 @@ import {
   Title,
   Tooltip,
 } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import './ExpenseStatistics.scss';
 
-ChartJS.register(ArcElement, Tooltip, Legend, Title, CategoryScale);
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+  CategoryScale,
+  ChartDataLabels
+);
+
+const data = {
+  labels: ['Bill Expense', 'Others', 'Investments', 'Entertainment'],
+  datasets: [
+    {
+      data: [15, 35, 20, 30],
+      backgroundColor: ['#fc7900', '#232323', '#396aff', '#343c6a'],
+      hoverOffset: 30,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  borderWidth: 4,
+  plugins: {
+    datalabels: {
+      color: 'white',
+      formatter: (value: any, ctx: any) => {
+        const label = ctx.chart.data.labels[ctx.dataIndex];
+        return `${value}%\n${label}`;
+      },
+      font: {
+        weight: 'bold' as 'bold',
+        size: 14,
+      },
+      padding: 6,
+      textAlign: 'center' as 'center',
+    },
+    legend: {
+      display: false,
+    },
+  },
+  layout: {
+    padding: 10,
+  },
+};
 
 const ExpenseStatistics: React.FC = () => {
-  const data = {
-    labels: ['Entertainment', 'Bills', 'Investments', 'Others'],
-    datasets: [
-      {
-        data: [40, 30, 20, 10],
-
-        backgroundColor: ['#f39c12', '#8e44ad', '#2ecc71', '#e74c3c'],
-        borderWidth: 1,
-      },
-    ],
-  };
-
   return (
     <div className="expense-statistics">
-      <h2>Expense Breakdown</h2>
-      <Pie data={data} />
+      <h3 className="font-semibold text-xl mb-4">Expense Statistics</h3>
+      <div className="chart-container rounded-xxl bg-white p-5">
+        <Pie data={data} options={options} />
+      </div>
     </div>
   );
 };
