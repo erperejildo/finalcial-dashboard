32eea41805a5bd69f1e5a189e5d8a5fa0410c239
Daniel <d15_1_89@msn.com>
2024-11-29 17:17:48 +0100

    mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
--- src/components/QuickTransfer.tsx
+++ src/components/QuickTransfer.tsx
@@ -102,96 +102,93 @@ const QuickTransfer: React.FC = () => {
     <div className="quick-transfer component">
       <h3 className="font-semibold text-xl mb-4">Quick Transfer</h3>
       <div className="rounded-xxl p-5">
-        <div className="quick-transfer component rounded-xxl">
-          <div className="contacts-container mb-4 gap-5">
-            {showArrows && (
-              <button
-                onClick={() => scrollContainer('left')}
-                className={`${showLeftArrow ? '' : 'hidden-arrow'} arrow p-2 rounded-full`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path d="M15 19l-7-7 7-7" />
-                </svg>
-              </button>
-            )}
-            <div
-              className={`contacts overflow-x-auto whitespace-nowrap ${showArrows ? 'overflow-hidden' : 'overflow-x-auto'}`}
-              ref={containerRef}
+        <div className="contacts-container gap-5">
+          {showArrows && (
+            <button
+              onClick={() => scrollContainer('left')}
+              className={`${showLeftArrow ? '' : 'hidden-arrow'} arrow p-2 rounded-full`}
             >
-              <div className="flex">
-                {contacts.map((contact, index) => (
-                  <div
-                    key={index}
-                    className="contact"
-                    onClick={() => selectContact(index)}
-                  >
-                    <img
-                      src={contact.profilePicture}
-                      alt={contact.name}
-                      className="contact-img rounded-full"
-                    />
-                    <div
-                      className={`${contact.selected ? 'contact-important ' : ''}`}
-                    >
-                      <p>{contact.name}</p>
-                      <p className="role text-sm">{contact.role}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-            {showArrows && (
-              <button
-                onClick={() => scrollContainer('right')}
-                className={`${showRightArrow ? '' : 'hidden-arrow'} arrow p-2 rounded-full`}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  className="w-6 h-6"
+                <path d="M15 19l-7-7 7-7" />
+              </svg>
+            </button>
+          )}
+          <div
+            className={`contacts overflow-x-auto whitespace-nowrap ${showArrows ? 'overflow-hidden' : 'overflow-x-auto'}`}
+            ref={containerRef}
+          >
+            <div className="flex">
+              {contacts.map((contact, index) => (
+                <div
+                  key={index}
+                  className="contact"
+                  onClick={() => selectContact(index)}
                 >
-                  <path d="M9 5l7 7-7 7" />
-                </svg>
-              </button>
-            )}
-          </div>
-
-          <div className="amount-container input-group flex items-center">
-            <span className="whitespace-nowrap">Write Amount</span>
-            <div className="amount-input-container w-full rounded-full flex items-center justify-between">
-              <input
-                type="number"
-                placeholder="Amount"
-                className="pl-6 pr-2 text-sm border-none outline-none flex-1"
-              />
-              <button className="bg-black text-white text-sm font-bold py-2 px-6 rounded-full flex items-center">
-                Send
-                <svg
-                  className="h-8 w-8 ml-2"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
-                </svg>
-              </button>
+                  <img
+                    src={contact.profilePicture}
+                    alt={contact.name}
+                    className="contact-img rounded-full"
+                  />
+                  <div
+                    className={`${contact.selected ? 'contact-important ' : ''}`}
+                  >
+                    <p>{contact.name}</p>
+                    <p className="role text-sm">{contact.role}</p>
+                  </div>
+                </div>
+              ))}
             </div>
+          </div>
+          {showArrows && (
+            <button
+              onClick={() => scrollContainer('right')}
+              className={`${showRightArrow ? '' : 'hidden-arrow'} arrow p-2 rounded-full`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path d="M9 5l7 7-7 7" />
+              </svg>
+            </button>
+          )}
+        </div>
+
+        <div className="amount-container input-group flex items-center">
+          <span className="whitespace-nowrap">Write Amount</span>
+          <div className="amount-input-container w-full rounded-full flex items-center justify-between">
+            <input
+              type="number"
+              placeholder="Amount"
+              className="pl-6 pr-2 text-sm border-none outline-none flex-1"
+            />
+            <button className="bg-black text-white text-sm font-bold py-2 px-6 rounded-full flex items-center">
+              Send
+              <svg
+                className="h-8 w-8 ml-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
